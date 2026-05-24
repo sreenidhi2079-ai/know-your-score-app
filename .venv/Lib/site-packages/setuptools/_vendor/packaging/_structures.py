c0fd8d8d37a96f21dd5402b5bcab7aa8905c5e35
sreenidhi2079-ai <your-github-email@gmail.com>
2026-05-24 19:51:11 +0530

    Your commit message

--- .venv/Lib/site-packages/setuptools/_vendor/packaging/_structures.py
+++ .venv/Lib/site-packages/setuptools/_vendor/packaging/_structures.py
@@ -1,68 +1,69 @@
 # This file is dual licensed under the terms of the Apache License, Version
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
-from __future__ import absolute_import, division, print_function
+
+import typing
 
 
-class Infinity(object):
+@typing.final
+class InfinityType:
+    __slots__ = ()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Infinity"
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(repr(self))
 
-    def __lt__(self, other):
+    def __lt__(self, other: object) -> bool:
         return False
 
-    def __le__(self, other):
+    def __le__(self, other: object) -> bool:
         return False
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         return isinstance(other, self.__class__)
 
-    def __ne__(self, other):
-        return not isinstance(other, self.__class__)
-
-    def __gt__(self, other):
+    def __gt__(self, other: object) -> bool:
         return True
 
-    def __ge__(self, other):
+    def __ge__(self, other: object) -> bool:
         return True
 
-    def __neg__(self):
+    def __neg__(self: object) -> "NegativeInfinityType":
         return NegativeInfinity
 
-Infinity = Infinity()
+
+Infinity = InfinityType()
 
 
-class NegativeInfinity(object):
+@typing.final
+class NegativeInfinityType:
+    __slots__ = ()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "-Infinity"
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(repr(self))
 
-    def __lt__(self, other):
+    def __lt__(self, other: object) -> bool:
         return True
 
-    def __le__(self, other):
+    def __le__(self, other: object) -> bool:
         return True
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         return isinstance(other, self.__class__)
 
-    def __ne__(self, other):
-        return not isinstance(other, self.__class__)
-
-    def __gt__(self, other):
+    def __gt__(self, other: object) -> bool:
         return False
 
-    def __ge__(self, other):
+    def __ge__(self, other: object) -> bool:
         return False
 
-    def __neg__(self):
+    def __neg__(self: object) -> InfinityType:
         return Infinity
 
-NegativeInfinity = NegativeInfinity()
+
+NegativeInfinity = NegativeInfinityType()
